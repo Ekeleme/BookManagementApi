5343e31159d3150c420b4b5073107be2bb599da9
Daniel Ekeleme <88858152+Ekeleme@users.noreply.github.com>
2021-12-29 20:10:22 +0100

    Add files via upload
    Handled unhandled exceptions.

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -28,13 +28,20 @@ namespace BookManagementApi.Controllers
         [HttpGet("GetBookById/{id}")]
         public ActionResult GetBookById(int id)
         {
-            if(id <= 0)
+            try
             {
-                NotFound();
-            }
-            var items = _context.BookItems.FirstOrDefault(x => x.Id == id);
+                if(id <= 0)
+                {
+                    NotFound();
+                }
+                var items = _context.BookItems.FirstOrDefault(x => x.Id == id);
 
-            return Ok(items);
+                return Ok(items);
+            }
+            catch
+            {
+                return BadRequest();
+            }
         }
 
         [HttpGet("GetBookByAuthor/{author}")]
@@ -66,42 +73,57 @@ namespace BookManagementApi.Controllers
         [HttpPut]
         public ActionResult UpdateBook([FromQuery] int idNumber, [FromBody] BookItems model)
         {
-            if(idNumber <= 0)
+            
+            try
             {
-                return NotFound();
+                if(idNumber <= 0)
+                {
+                    return NotFound();
+                }
+
+                var item = _context.BookItems.FirstOrDefault(x => x.Id == idNumber);
+
+                if(item == null)
+                {
+                    return BadRequest();
+                }
+
+                item.Name = model.Name;
+                item.Description = model.Description;
+                item.Author = model.Author;
+                model.DateAdded = DateTime.Now;
+                item.DateAdded = model.DateAdded;
+
+                _context.BookItems.Update(item);
+                _context.SaveChanges();
+
+                return NoContent();
             }
-
-            var item = _context.BookItems.FirstOrDefault(x => x.Id == idNumber);
-
-            if(item == null)
+            catch
             {
                 return BadRequest();
             }
-
-            item.Name = model.Name;
-            item.Description = model.Description;
-            item.Author = model.Author;
-            model.DateAdded = DateTime.Now;
-            item.DateAdded = model.DateAdded;
-
-            _context.BookItems.Update(item);
-            _context.SaveChanges();
-
-            return NoContent();
         }
 
         [HttpDelete("DeleteBookById/{id}")]
         public ActionResult DeleteBook(int id)
         {
-            if (id <= 0)
+            try
             {
-                return NotFound();
+                if (id <= 0)
+                {
+                    return NotFound();
+                }
+
+                var item = _context.BookItems.FirstOrDefault(x => x.Id == id);
+                _context.BookItems.Remove(item);
+
+                return NoContent();
             }
-
-            var item = _context.BookItems.FirstOrDefault(x => x.Id == id);
-            _context.BookItems.Remove(item);
-
-            return NoContent();
+            catch
+            {
+                return BadRequest();
+            }
         }
 
 
